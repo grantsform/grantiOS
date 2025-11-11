8e5a8dd0bd356227be543a4d6f3319519fb93fc5
Joshua Steven Grant (ie: Jost Grant) <src.grant@pm.me>
2025-11-11 02:43:15 -0600

    Let's see if this works. lol

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,26 @@
 
 set -ouex pipefail
 
+# Update system and install packages
+dnf5 update -y
+
 dnf5 install -y zsh git git-lfs gum tmux btop rsync restic rclone syncthing micro godot chibi-scheme chez-scheme sbcl papirus-icon-theme 
 
+dnf5 install -y dnf5-plugins
+
+# Import Microsoft key and add VS Code repo
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+# Add GitHub CLI repo
+dnf5 config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
+
+# Add Tailscale repo
+dnf5 config-manager --add-repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+
+# Install third-party packages
+dnf5 install -y code gh tailscale
+
+# Enable Services, Sockets, and Timers
 systemctl enable podman.socket
+systemctl enable tailscaled
